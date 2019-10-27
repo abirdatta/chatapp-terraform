903cc5a562ec9582b1fc2e9051e0d538c4f598d6
Abir Datta <abirdatta.in@gmail.com>
2019-10-27 13:36:22 +0530

    added output subnet cidrs

--- resources/SUBNETS/outputs.tf
+++ resources/SUBNETS/outputs.tf
@@ -2,6 +2,10 @@ output "subnet_ids" {
   value = [for subnet in aws_subnet.subnets: subnet.id]
 }
 
+output "subnet_cidrs" {
+  value = [for subnet in aws_subnet.subnets: subnet.cidr_block]
+}
+
 output "az" {
   value = [for subnet in aws_subnet.subnets: subnet.availability_zone]
 }
